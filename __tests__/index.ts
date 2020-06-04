8f064c89c52e332816c696d2ea881ee4b21a353a
Sergei Zyubin <sergei.zyubin@gmail.com>
2020-06-04 10:49:57 +0200

    test(tagformat): 🚨 update the test for a new tagFormat

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -2,5 +2,5 @@ import { tagFormat } from "../src"
 
 // Jest doesn't honor require.cache, so it's impossible to test this stuff
 test("tagFormat", () => {
-  expect(tagFormat).toMatch(/^semantic-release-commit-filter-v\$\{version\}$/u)
+  expect(tagFormat).toMatch(/^pkg\/semantic-release-commit-filter\/v\$\{version\}$/u)
 })
