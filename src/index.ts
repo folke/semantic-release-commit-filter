585524042ed490766bba3bd858feef5d0fecb2c5
Bruno Trigueiro <bruno.trigueiro@finleap.com>
2020-07-28 18:53:17 +0200

    feat: adds local dependency paths to parse paths

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
 import { posix } from "path"
+import { existsSync } from "fs"
+
+function findLocalDependencyPaths(cwd: string): string[] {
+  const filePathPrefix = "file:"
+  const absPath = posix.resolve(process.cwd(), cwd.replace(filePathPrefix, ""))
+  const absPackageJsonPath = posix.resolve(absPath, "package.json")
+
+  if (!existsSync(absPackageJsonPath)) return []
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { dependencies = {} } = require(absPackageJsonPath)
+
+  return Object.entries(dependencies as { [key: string]: string })
+    .filter(([, path]) => path.startsWith(filePathPrefix))
+    .map(([, path]) => findLocalDependencyPaths(path))
+    .reduce((acc, paths) => [...acc, ...paths], [absPath])
+}
 
 Object.keys(require.cache)
   .filter(m =>
@@ -12,7 +28,14 @@ Object.keys(require.cache)
     ) => {
       if (Array.isArray(config._)) config._.push(options.cwd)
       else if (config._) config._ = [config._, options.cwd]
-      else config._ = options.cwd
+      else config._ = [options.cwd]
+
+      const foundPaths = config._.map(path =>
+        findLocalDependencyPaths(path)
+      ).reduce((acc, paths) => [...acc, ...paths], [])
+      const uniquePaths = [...new Set(foundPaths)]
+      config._ = [...config._, ...uniquePaths]
+
       return parse(config, options)
     }
   })
