a21832a6a50cd87ae970faaac0616bf47ada0166
Bruno Trigueiro <bruno.trigueiro@finleap.com>
2020-08-03 00:45:18 +0200

    fix: 🐛 makes it safe for use with cyclic dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,35 @@
 import { posix } from "path"
 import { existsSync } from "fs"
 
-function findLocalDependencyPaths(cwd: string): string[] {
-  const filePathPrefix = "file:"
-  const absPath = posix.resolve(process.cwd(), cwd.replace(filePathPrefix, ""))
+const PROCESS_CWD = process.cwd()
+const filePathPrefix = "file:"
+
+function resolveAbsPath(path: string) {
+  return posix.resolve(PROCESS_CWD, path.replace(filePathPrefix, ""))
+}
+
+function findLocalDependencyPaths(
+  cwd: string,
+  resolvedPaths: Set<string>
+): string[] {
+  const absPath = resolveAbsPath(cwd)
   const absPackageJsonPath = posix.resolve(absPath, "package.json")
 
   if (!existsSync(absPackageJsonPath)) return []
+  resolvedPaths.add(absPath)
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const { dependencies = {} } = require(absPackageJsonPath)
 
   return Object.entries(dependencies as { [key: string]: string })
-    .filter(([, path]) => path.startsWith(filePathPrefix))
-    .map(([, path]) => findLocalDependencyPaths(path))
-    .reduce((acc, paths) => [...acc, ...paths], [absPath])
+    .map(([, path]) => [path, resolveAbsPath(path)])
+    .filter(
+      ([path, resolvedAbsPath]) =>
+        path.startsWith(filePathPrefix) && !resolvedPaths.has(resolvedAbsPath)
+    )
+    .flatMap(([, resolvedAbsPath]) => {
+      resolvedPaths.add(resolvedAbsPath)
+      return findLocalDependencyPaths(resolvedAbsPath, resolvedPaths)
+    })
 }
 
 Object.keys(require.cache)
@@ -30,16 +46,16 @@ Object.keys(require.cache)
       else if (config._) config._ = [config._, options.cwd]
       else config._ = [options.cwd]
 
-      const foundPaths = config._.map(path =>
-        findLocalDependencyPaths(path)
-      ).reduce((acc, paths) => [...acc, ...paths], [])
-      const uniquePaths = [...new Set(foundPaths)]
-      config._ = [...config._, ...uniquePaths]
+      const resolvedPaths = new Set<string>()
+      config._.flatMap(path =>
+        findLocalDependencyPaths(resolveAbsPath(path), resolvedPaths)
+      )
+      config._ = [...config._, ...resolvedPaths]
 
       return parse(config, options)
     }
   })
 
 export const tagFormat = `${
-  require(posix.resolve(process.cwd(), "package.json")).name
+  require(posix.resolve(PROCESS_CWD, "package.json")).name
 }-v\${version}`
